56688088065cfcd39dc4377690fc7621c7d4040a
marshmellow_potato <70851931+XU3N@users.noreply.github.com>
2025-12-12 04:10:55 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -10,7 +10,7 @@ bucket = custombucket
 region = customregion
 
 db_conn = connections.Connection(
-    host=employee.cpkwmc0uo77c.us-east-1.rds.amazonaws.com,
+    host=customhost,
     port=3306,
     user=customuser,
     password=custompass,
